10d0c4c8a98d6b177b115102d5eea2dbd396fe5a
saurav <tsaurav18@gmail.com>
2021-01-21 16:27:01 +0900

    make it beter

--- index.js
+++ index.js
@@ -11,14 +11,15 @@ app.use(bodyParser.urlencoded({limit:"30mb", extended:true}))
 app.use(cors())
 
 app.use('/post', postRouters)
+app.get('/', (req,res)=>{
+    res.send("hello to memories api")})
 //mongodb
-// const CONNECTION_URL = "mongodb+srv://admin:admin123123@cluster0.nokoi.mongodb.net/memories?retryWrites=true&w=majority"
+
 const PORT  = process.env.PORT || 5000;
  mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology:true}).then(()=>{
      app.listen(PORT,()=>{console.log(`server running on port:${PORT}`)})
  }).catch((error=>{console.log(error.message)}))
-app.get('/', (req,res)=>{
-    res.send("hello to memories api")})
+
  mongoose.set("useFindAndModify", false)
 
 
